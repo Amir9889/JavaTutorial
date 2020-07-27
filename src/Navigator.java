22beff82ccad7197133fdbcd2559323502372b32
Amir Mehrabi <chenar1397@gmail.com>
2020-07-27 12:40:09 +0430

    Initial commit + Vaiables

---
+++ src/Navigator.java
@@ -0,0 +1,13 @@
+import ir.porteqali.javatutorial.E016_Variables;
+
+/**
+ * This class navigate us to the target classes
+ */
+public class Navigator {
+
+    public static void main(String[] args){
+        System.out.println("Navigator");
+        E016_Variables variables=new E016_Variables();
+        variables.start();
+    }
+}
---
+++ src/ir/porteqali/javatutorial/E016_Variables.java
@@ -0,0 +1,26 @@
+package ir.porteqali.javatutorial;
+
+public class E016_Variables {
+
+    public void start(){
+        System.out.println(getClass().getSimpleName());
+        // integer
+        int a = 10;
+        short b = 15;
+        byte c = 5;
+        long d = 22;
+        // decimal
+        float e = 12.3f;
+        double f = 222.67;
+
+        // character
+        char g =255;
+        char h = 'a';
+        // String :
+        String i =" klaafjsanfjasnf\naksndkls\tjsnjn \"sfas\"SF";
+        System.out.println(i);
+        // boolean
+        boolean j = false;
+
+    }
+}
