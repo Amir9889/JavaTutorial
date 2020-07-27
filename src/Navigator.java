844064ec59ebe5c8978731f7d229519bf2b67474
Amir Mehrabi <chenar1397@gmail.com>
2020-07-27 13:02:05 +0430

    E017_NamingRules

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,4 +1,5 @@
 import ir.porteqali.javatutorial.E016_Variables;
+import ir.porteqali.javatutorial.E017_NamingRules;
 
 /**
  * This class navigate us to the target classes
@@ -6,8 +7,11 @@ import ir.porteqali.javatutorial.E016_Variables;
 public class Navigator {
 
     public static void main(String[] args){
+        // showing "Navigator" to know we are using Navigator class to find our other classes
         System.out.println("Navigator");
-        E016_Variables variables=new E016_Variables();
-        variables.start();
+
+        // Creating an instance of E017_NamingRules class and call start() :
+        E017_NamingRules rules=new E017_NamingRules();
+        rules.start();
     }
 }
---
+++ src/ir/porteqali/javatutorial/E017_NamingRules.java
@@ -0,0 +1,21 @@
+package ir.porteqali.javatutorial;
+
+public class E017_NamingRules {
+
+    public void start(){
+        // this method show "E017_NamingRules" to know we are working in this class
+        System.out.println(getClass().getSimpleName());
+
+        /*
+        Naming rules for variables:
+        1 : The variable name must specify its functionality
+        2 : We can't use 0 to first of its name
+        3 : We should write by camleCase or us underlines ( _ )
+         */
+
+        int myAge = 18 ;
+        String myNameAndLastname="Amir Mehrabi";
+        boolean amI20 = false;
+
+    }
+}
