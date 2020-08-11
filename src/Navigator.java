ee9c278f3a92de3bd3df0a01bd6409532e612fd5
Amir Mehrabi <chenar1397@gmail.com>
2020-08-11 10:41:08 +0430

    E020_TryCatchBlock

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,7 +1,5 @@
 
-import ir.porteqali.javatutorial.E018_Keywords;
-import ir.porteqali.javatutorial.E019_CastingVariables;
-import ir.porteqali.javatutorial.E020_TryCatchBlock;
+import ir.porteqali.javatutorial.*;
 
 /**
  * This class navigate us to the target classes
@@ -12,9 +10,9 @@ public class Navigator {
         // showing "Navigator" to know we are using Navigator class to find our other classes
         System.out.println("Navigator");
 
-        // Creating an instance of E020_TryCatchBlock class and call start() :
-        E020_TryCatchBlock tryCatchBlock=new E020_TryCatchBlock();
-        tryCatchBlock.start();
+        // Creating an instance of E021_ArithmeticOperators class and call start() :
+        E021_ArithmeticOperators arithmeticOperators=new E021_ArithmeticOperators();
+        arithmeticOperators.start();
 
 
     }
---
+++ src/ir/porteqali/javatutorial/E021_ArithmeticOperators.java
@@ -0,0 +1,29 @@
+package ir.porteqali.javatutorial;
+
+
+public class E021_ArithmeticOperators {
+
+    public void start(){
+        System.out.println(getClass().getSimpleName());
+
+        /*
+
+        +
+        -
+        /
+        *
+        %
+        ++
+        --
+
+         */
+
+        int a=25;
+        int b=8;
+        System.out.println(a%b);
+
+        int c=8;
+        System.out.println(c++);
+        System.out.println(c);
+    }
+}
